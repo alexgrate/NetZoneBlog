7907ecbc01322e364310d6e768c7f5708da34194
Aigbodion Dominion <116392457+alexgrate@users.noreply.github.com>
2024-03-26 06:53:03 +0100

    Config js file/Added movies

--- Netblog/static/js/script.js
+++ Netblog/static/js/script.js
@@ -33,10 +33,9 @@ function closeSidebarAndSearchForm() {
     searchForm.classList.remove('active');
 }
 
-window.addEventListener('scroll', () => {
+window.onscroll = () => {
     closeSidebarAndSearchForm();
-})
-
+}
 
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
