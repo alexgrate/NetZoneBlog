8e4fbce6811f0908bc63fd33e8161a3664340603
Aigbodion Dominion <116392457+alexgrate@users.noreply.github.com>
2024-04-13 14:27:01 +0100

    added ads

--- Netblog/views.py
+++ Netblog/views.py
@@ -305,7 +305,7 @@ def search(request):
 def ads_txt(request):
     ads_content = """
     
-    google.com, pub-8616826559084236, DIRECT, f08c47fec0942fa0
+google.com, pub-8616826559084236, DIRECT, f08c47fec0942fa0
 
     ### ForeMedia START ###
 google.com, pub-1444749628606253, DIRECT, f08c47fec0942fa0
