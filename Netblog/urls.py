626decd68b821a50742963762085a0854a7941bd
Aigbodion Dominion <116392457+alexgrate@users.noreply.github.com>
2024-08-28 07:24:15 +0100

    Configured uRl

--- Netblog/urls.py
+++ Netblog/urls.py
@@ -13,7 +13,4 @@ urlpatterns = [
     path('NetBlog/Tag/Category/<str:name>', views.tag_categories, name= 'Netblog-Tags-Categories'),
     path('NetBlog/Date/Category/<str:movie_date>', views.date_categories, name= 'Netblog-Date-Categories'),
     path('NetBlog/Season/Category/<str:movie_title>', views.seasons_categories, name= 'Netblog-Season-Categories'),
-    path('ads.txt', views.ads_txt, name='ads_txt'),
-    path('sw.js', views.sw_txt, name='sw_js'),
-    path('robots.txt', views.robots_txt, name='robots_txt'),
 ]
